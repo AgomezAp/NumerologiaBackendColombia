9889e5707af5806ba696b5f92cf294790ac158f5
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-03-21 16:15:13 -0500

    add initial lodash and moment module files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,3 @@ import Server from './config.js';
 
 dotenv.config();
 const server = new Server();
-/* 
-const app = express();
-app.use(morgan('dev'));    
-app.use(express.json());
-app.use(paymentRoutes); 
-
-app.listen(PORT);
-
-console.log('Server is running on port ', PORT); */
